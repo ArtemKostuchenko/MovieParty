b6b0fd70a63abcfd2b493ecf144fdd1fbc543e91
KostArt <ipz204_kav@student.ztu.edu.ua>
2023-12-01 20:28:41 +0200

    Added middleware to handle errors in controllers

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
+require('express-async-errors');
 const express = require('express');
 const connectToDB = require('./db/connection');
 const authRouter = require('./routes/auth');
+const errorMiddleware = require('./middleware/error');
 
 const app = express();
 
 // routers
 
 app.use('/api/v1/auth', authRouter);
+
+// middleware
+
+app.use(errorMiddleware);
 
 const port = 5000 || process.env.PORT;
 
