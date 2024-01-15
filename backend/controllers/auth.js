1ed94e36e9d50a9fbf3a7ba61596a90e8f9e6bc2
KostArt <ipz204_kav@student.ztu.edu.ua>
2024-01-15 14:05:27 +0200

    Implemented the ability to reset a password without mail

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,9 @@
 const { BadRequestError, UnAuthorizedError, NotFoundError } = require("../errors");
-const UserModel = require('../models/user');
 const { StatusCodes } = require('http-status-codes');
+const TokenModel = require('../models/token');
+const UserModel = require('../models/user');
+const bcrypt = require('bcryptjs');
+const crypto = require("crypto");
 
 const register = async (req, res) => {
     const { nickname, email, password } = req.body;
@@ -41,9 +44,71 @@ const login = async (req, res) => {
 }
 
 const getMe = async (req, res) => {
-    const user = await UserModel.findById(req.user.id).select('-password');
+    const user = await UserModel.findById(req.user.userId).select('-password');
 
     return res.status(StatusCodes.OK).json({ user });
+}
+
+const reqPasswordReset = async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+        throw new BadRequestError("Please provide email");
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+        throw new NotFoundError("User not found");
+    }
+
+    const token = await TokenModel.findOne({ userId: user._id });
+
+    if (token) {
+        await token.deleteOne();
+    }
+
+    let resetToken = crypto.randomBytes(32).toString("hex");
+
+    const salt = await bcrypt.genSalt(10);
+    const hashToken = await bcrypt.hash(resetToken, salt);
+
+    await TokenModel.create({ userId: user._id, token: hashToken, createdAt: Date.now() });
+
+    const link = `${process.env.CLIENT_URL}/password-reset?token=${resetToken}&id=${user._id}`;
+
+    return res.status(StatusCodes.OK).json({ success: true, link });
+}
+
+const resetPassword = async (req, res) => {
+    const { userId, token, password } = req.body;
+
+    if (!userId || !token, !password) {
+        throw new BadRequestError("Please provide userId, token and password");
+    }
+
+    const resetToken = await TokenModel.findOne({ userId });
+
+    if (!resetToken) {
+        throw new UnAuthorizedError("Invalid or expired password reset token");
+    }
+
+    const validToken = await bcrypt.compare(token, resetToken.token);
+
+    if (!validToken) {
+        throw new UnAuthorizedError("Invalid or expired password reset token");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await UserModel.updateOne({ _id: userId }, { $set: { password: hashedPassword } }, { new: true });
+
+    const user = await UserModel.findById(userId).select('-password');
+
+    await resetToken.deleteOne();
+
+    return res.status(StatusCodes.OK).json({ success: true, user });
 }
 
 
@@ -51,4 +116,6 @@ module.exports = {
     register,
     login,
     getMe,
+    reqPasswordReset,
+    resetPassword,
 }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/auth');
+const { register, login, getMe, reqPasswordReset, resetPassword } = require('../controllers/auth');
 const { authMiddleware } = require('../middleware/auth');
 
 const router = express.Router();
@@ -7,6 +7,8 @@ const router = express.Router();
 // post
 router.post('/register', register);
 router.post('/login', login);
+router.post('/password/req-reset', reqPasswordReset);
+router.post('/password/reset', resetPassword)
 router.get('/me', authMiddleware, getMe);
 
 
