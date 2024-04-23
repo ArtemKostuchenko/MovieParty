e0bdf461d5ac2f7eaeaf26776d550ba50784a198
KostArt <ipz204_kav@student.ztu.edu.ua>
2024-04-23 11:31:00 +0300

    Change route method from get to post for logout route

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,20 +1,37 @@
-import express, { Router } from 'express';
-import { register, login, getMe, reqPasswordReset, resetPassword, logOut } from '../controllers/auth.controller';
-import { authMiddleware } from '../middlewares/auth.middleware';
-import passport from 'passport';
+import express, { Router } from "express";
+import {
+  register,
+  login,
+  getMe,
+  reqPasswordReset,
+  resetPassword,
+  logOut,
+} from "../controllers/auth.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import passport from "passport";
 
 const router: Router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post("/register", register);
+router.post("/login", login);
 
 // google oauth2.0
-router.get('/login/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
-router.get('/google/callback', passport.authenticate("google", { failureMessage: "Cannot login to Google, please try again later", failureRedirect: 'http://localhost:3000/login/fail', successRedirect: 'http://localhost:3000/login/success' }));
+router.get(
+  "/login/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureMessage: "Cannot login to Google, please try again later",
+    failureRedirect: "http://localhost:3000/login/fail",
+    successRedirect: "http://localhost:3000/login/success",
+  })
+);
 
-router.post('/password/req-reset', reqPasswordReset);
-router.post('/password/reset', resetPassword);
-router.get('/me', authMiddleware, getMe);
-router.get('/logout', logOut);
+router.post("/password/req-reset", reqPasswordReset);
+router.post("/password/reset", resetPassword);
+router.get("/me", authMiddleware, getMe);
+router.post("/logout", logOut);
 
 export default router;
