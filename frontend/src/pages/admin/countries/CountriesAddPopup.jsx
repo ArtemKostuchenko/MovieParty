81b8b68eaced34e48af291fb01d63c6afb5eca40
KostArt <ipz204_kav@student.ztu.edu.ua>
2024-05-14 16:54:19 +0300

    The logic of adding a country to a separate component has been moved

---
+++ frontend/src/pages/admin/countries/CountriesAddPopup.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import PopUp from "../../../components/PopUp/PopUp";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { CountrySchema } from "../../../features/validations";
+import { PreviewImage } from "../../../components";
+import useCountry from "../../../hooks/useCountry";
+
+const CountriesAddPopup = ({ isOpen, setIsOpen }) => {
+  const { addCountry } = useCountry();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    resetField,
+    watch,
+    formState: { errors, isDirty, isValid },
+  } = useForm({
+    resolver: yupResolver(CountrySchema),
+  });
+
+  const onSubmitHandler = async (data) => {
+    const res = await addCountry(data);
+    console.log(res);
+    setIsOpen(false);
+    reset();
+  };
+
+  const resetIcon = () => {
+    resetField("icon");
+  };
+
+  const watchIcon = watch("icon");
+
+  return (
+    <PopUp title="Додавання країни" open={isOpen} setOpen={setIsOpen}>
+      <div className="popup__form">
+        <form className="form" onSubmit={handleSubmit(onSubmitHandler)}>
+          <div className="popup__form-items">
+            <div className="popup__form-item">
+              <div className="popup__form-title">Назва країни</div>
+              <input
+                {...register("name")}
+                type="text"
+                className="form__input linear"
+              />
+              {errors.name && (
+                <span className="message error">{errors.name.message}</span>
+              )}
+            </div>
+            <div className="popup__form-item">
+              <div className="popup__form-title">Оригінальна назва країни</div>
+              <input
+                type="text"
+                {...register("originName")}
+                className="form__input linear"
+              />
+              {errors.originName && (
+                <span className="message error">
+                  {errors.originName.message}
+                </span>
+              )}
+            </div>
+            <div className="popup__form-item">
+              <div className="popup__form-title">Іконку країни (25x20)</div>
+              {!Boolean(watchIcon?.length) ? (
+                <button
+                  type="button"
+                  className="button primary fill"
+                  onClick={() => {
+                    const inputRef =
+                      document.querySelector('input[name="icon"]');
+                    if (!inputRef) return;
+                    inputRef.click();
+                  }}
+                >
+                  Обрати іконку
+                </button>
+              ) : (
+                <PreviewImage file={watchIcon[0]} removeIcon={resetIcon} />
+              )}
+
+              <input
+                type="file"
+                {...register("icon")}
+                className="hidden"
+                accept="image/*"
+              />
+            </div>
+            <button
+              type="submit"
+              className="button primary fill"
+              disabled={!isDirty || !isValid}
+            >
+              Додати
+            </button>
+          </div>
+        </form>
+      </div>
+    </PopUp>
+  );
+};
+
+export default CountriesAddPopup;
--- frontend/src/pages/admin/countries/CountriesPage.jsx
+++ frontend/src/pages/admin/countries/CountriesPage.jsx
@@ -1,39 +1,12 @@
 import React, { useState } from "react";
 import "./style.page.scss";
-import PopUp from "../../../components/PopUp/PopUp";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { CountrySchema } from "../../../features/validations";
 import useCountry from "../../../hooks/useCountry";
-import { CountryList, PreviewImage } from "../../../components";
+import CountriesAddPopup from "./CountriesAddPopup";
+import { CountryList } from "../../../components";
 
 const CountryPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const { addCountry, isSuccessAdd, isLoadingAdd } = useCountry();
-
-  const {
-    register,
-    handleSubmit,
-    reset,
-    resetField,
-    watch,
-    formState: { errors, isDirty, isValid },
-  } = useForm({
-    resolver: yupResolver(CountrySchema),
-  });
-
-  const onSubmitHandler = async (data) => {
-    const res = await addCountry(data);
-    console.log(res);
-    setIsOpen(false);
-    reset();
-  };
-
-  const resetIcon = () => {
-    resetField("icon");
-  };
-
-  const watchIcon = watch("icon");
+  const [isOpenAdd, setIsOpenAdd] = useState(false);
+  const { isLoadingAdd } = useCountry();
 
   return (
     <>
@@ -44,8 +17,8 @@ const CountryPage = () => {
             <div className="view-actions">
               <button
                 className="button primary"
-                disabled={isOpen || isLoadingAdd}
-                onClick={() => setIsOpen(!isOpen)}
+                disabled={isOpenAdd || isLoadingAdd}
+                onClick={() => setIsOpenAdd(!isOpenAdd)}
               >
                 Додати країну
               </button>
@@ -64,74 +37,8 @@ const CountryPage = () => {
           </div>
         </div>
       </div>
-      {isOpen && (
-        <PopUp title="Додавання країни" open={isOpen} setOpen={setIsOpen}>
-          <div className="popup__form">
-            <form className="form" onSubmit={handleSubmit(onSubmitHandler)}>
-              <div className="popup__form-items">
-                <div className="popup__form-item">
-                  <div className="popup__form-title">Назва країни</div>
-                  <input
-                    {...register("name")}
-                    type="text"
-                    className="form__input linear"
-                  />
-                  {errors.name && (
-                    <span className="message error">{errors.name.message}</span>
-                  )}
-                </div>
-                <div className="popup__form-item">
-                  <div className="popup__form-title">
-                    Оригінальна назва країни
-                  </div>
-                  <input
-                    type="text"
-                    {...register("originName")}
-                    className="form__input linear"
-                  />
-                  {errors.originName && (
-                    <span className="message error">
-                      {errors.originName.message}
-                    </span>
-                  )}
-                </div>
-                <div className="popup__form-item">
-                  <div className="popup__form-title">Іконку країни (25x20)</div>
-                  {!Boolean(watchIcon?.length) ? (
-                    <button
-                      type="button"
-                      className="button primary fill"
-                      onClick={() => {
-                        const inputRef =
-                          document.querySelector('input[name="icon"]');
-                        if (!inputRef) return;
-                        inputRef.click();
-                      }}
-                    >
-                      Обрати іконку
-                    </button>
-                  ) : (
-                    <PreviewImage file={watchIcon[0]} removeIcon={resetIcon} />
-                  )}
-
-                  <input
-                    type="file"
-                    {...register("icon")}
-                    className="hidden"
-                    accept="image/*"
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="button primary fill"
-                  disabled={!isDirty || !isValid}
-                >
-                  Додати
-                </button>
-              </div>
-            </form>
-          </div>
-        </PopUp>
+      {isOpenAdd && (
+        <CountriesAddPopup isOpen={isOpenAdd} setIsOpen={setIsOpenAdd} />
       )}
     </>
   );
