2d7106693932e586ba487033c3ab69fdde121644
KostArt <ipz204_kav@student.ztu.edu.ua>
2024-05-18 22:58:10 +0300

    Fix getGenres query

--- backend/src/controllers/genre.controller.ts
+++ backend/src/controllers/genre.controller.ts
@@ -1,47 +1,41 @@
-import { Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
-import GenreRepository from '../repositories/genre.repository';
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import GenreRepository from "../repositories/genre.repository";
 
 const createGenre = async (req: Request, res: Response): Promise<Response> => {
-    const genre = await GenreRepository.createGenre(req.body);
+  const genre = await GenreRepository.createGenre(req.body);
 
-    return res.status(StatusCodes.CREATED).json({ data: genre });
-}
+  return res.status(StatusCodes.CREATED).json({ data: genre });
+};
 
 const getGenre = async (req: Request, res: Response): Promise<Response> => {
-    const { id: idGenre } = req.params;
+  const { id: idGenre } = req.params;
 
-    const genre = await GenreRepository.getGenreById(idGenre);
+  const genre = await GenreRepository.getGenreById(idGenre);
 
-    return res.status(StatusCodes.OK).json({ data: genre });
-}
+  return res.status(StatusCodes.OK).json({ data: genre });
+};
 
 const updateGenre = async (req: Request, res: Response): Promise<Response> => {
-    const { id: idGenre } = req.params;
+  const { id: idGenre } = req.params;
 
-    const updatedGenre = await GenreRepository.updateGenreById(idGenre, req.body);
+  const updatedGenre = await GenreRepository.updateGenreById(idGenre, req.body);
 
-    return res.status(StatusCodes.OK).json({ data: updatedGenre });
-}
+  return res.status(StatusCodes.OK).json({ data: updatedGenre });
+};
 
 const deleteGenre = async (req: Request, res: Response): Promise<Response> => {
-    const { id: idGenre } = req.params;
+  const { id: idGenre } = req.params;
 
-    await GenreRepository.deleteGenreById(idGenre);
+  await GenreRepository.deleteGenreById(idGenre);
 
-    return res.status(StatusCodes.OK).json({ success: true });
-}
+  return res.status(StatusCodes.OK).json({ success: true });
+};
 
 const getGenres = async (req: Request, res: Response): Promise<Response> => {
-    const genres = await GenreRepository.getGenres(req.body);
+  const genres = await GenreRepository.getGenres(req.query);
 
-    return res.status(StatusCodes.OK).json({ data: genres });
-}
-
-export {
-    createGenre,
-    getGenre,
-    updateGenre,
-    deleteGenre,
-    getGenres,
+  return res.status(StatusCodes.OK).json({ data: genres });
 };
+
+export { createGenre, getGenre, updateGenre, deleteGenre, getGenres };
