7b62a151b949bf5fce562127888541f18e8aed75
KostArt <ipz204_kav@student.ztu.edu.ua>
2024-05-18 12:56:21 +0300

    Created useTypeContent hook with sorting and pagination

---
+++ frontend/src/hooks/useTypeContent.jsx
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  changePage,
+  resetPage as resPage,
+  changeSort,
+} from "../features/store/slices/type-content";
+
+const useTypeContent = () => {
+  const dispatch = useDispatch();
+
+  const { page, sortName, sortType } = useSelector((store) => store.country);
+
+  const onChangePage = (page) => {
+    dispatch(changePage(page));
+  };
+
+  const resetPage = () => {
+    dispatch(resPage());
+  };
+
+  const onChangeSort = (sortName, sortType) => {
+    dispatch(changeSort({ sortName, sortType }));
+  };
+
+  return {
+    page,
+    onChangePage,
+    resetPage,
+    sortName,
+    sortType,
+    onChangeSort,
+  };
+};
+
+export default useTypeContent;
