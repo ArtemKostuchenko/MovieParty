4ab6c62c8ecad0d45c37e7e63265625430751fa9
KostArt <ipz204_kav@student.ztu.edu.ua>
2024-05-18 11:43:32 +0300

    Create TypeContent repository, validation

---
+++ backend/src/repositories/type-content.repository.ts
@@ -0,0 +1,91 @@
+import TypeContentModel, { TypeContent } from "../models/type-content.model";
+import { validateTypeContent } from "../utils/validations";
+import { NotFoundError } from "../errors";
+
+interface Query {
+  name?: { $regex: string; $options: string };
+}
+
+class TypeContentRepository {
+  constructor() {}
+
+  async createTypeContent(typeContentData: TypeContent): Promise<TypeContent> {
+    validateTypeContent(typeContentData);
+
+    return await TypeContentModel.create(typeContentData);
+  }
+
+  async getTypeContentById(typeContentId: string): Promise<TypeContent> {
+    const typeContent = await TypeContentModel.findById(typeContentId);
+
+    if (!typeContent) {
+      throw new NotFoundError("TypeContent not found");
+    }
+
+    return typeContent;
+  }
+
+  async updateTypeContentById(
+    typeContentId: string,
+    typeContentData: TypeContent
+  ): Promise<TypeContent> {
+    const { name, path, isSeries } = typeContentData;
+
+    const typeContent = await TypeContentModel.findById(typeContentId);
+
+    if (!typeContent) {
+      throw new NotFoundError("TypeContent not found");
+    }
+
+    typeContent.name = name || typeContent.name;
+    typeContent.path = path || typeContent.path;
+    typeContent.isSeries = isSeries || typeContent.isSeries;
+
+    return await typeContent.save();
+  }
+
+  async deleteTypeContentById(typeContentId: string): Promise<void> {
+    const typeContent = await TypeContentModel.findById(typeContentId);
+
+    if (!typeContent) {
+      throw new NotFoundError("TypeContent not found");
+    }
+
+    await typeContent.deleteOne();
+  }
+
+  async getTypesContent(
+    query: any
+  ): Promise<{ typesContent: TypeContent[]; totalCount: number }> {
+    const { name, fields, sort } = query;
+
+    const queryObj: Query = {};
+
+    if (name) {
+      queryObj.name = { $regex: name, $options: "i" };
+    }
+
+    let typesContentQuery = TypeContentModel.find(queryObj);
+
+    if (sort) {
+      const sortList = sort.split(",").join(" ");
+      typesContentQuery = typesContentQuery.sort(sortList);
+    }
+
+    if (fields) {
+      const fieldList = fields.split(",").join(" ");
+      typesContentQuery = typesContentQuery.select(fieldList);
+    }
+
+    const typesContentPerPage = query.limit || 20;
+    const page = query.page || 1;
+    const skip = (page - 1) * typesContentPerPage;
+
+    const [typesContent, totalCount] = await Promise.all([
+      typesContentQuery.skip(skip).limit(typesContentPerPage),
+      TypeContentModel.countDocuments(queryObj),
+    ]);
+
+    return { typesContent, totalCount };
+  }
+}
--- backend/src/utils/validations.ts
+++ backend/src/utils/validations.ts
@@ -1,175 +1,198 @@
-import { BadRequestError } from '../errors';
-import { VideoContent } from '../models/content.model';
-import { Actor } from '../models/actor.model';
-import { Director } from '../models/director.model';
-import { Room } from '../models/room.model';
-import { Message } from '../models/message.model';
-
+import { BadRequestError } from "../errors";
+import { VideoContent } from "../models/content.model";
+import { Actor } from "../models/actor.model";
+import { Director } from "../models/director.model";
+import { Room } from "../models/room.model";
+import { Message } from "../models/message.model";
+import { TypeContent } from "../models/type-content.model";
 
 const emitErrors = (errors: string[]) => {
-    if (errors.length > 0) {
-        throw new BadRequestError(`Please provide ${errors.join(', ')}`);
-    }
-}
+  if (errors.length > 0) {
+    throw new BadRequestError(`Please provide ${errors.join(", ")}`);
+  }
+};
 
 const validateVideoContent = (videoContent: VideoContent) => {
-    const errors: string[] = [];
+  const errors: string[] = [];
 
-    if (!videoContent.title) {
-        errors.push('title');
-    }
+  if (!videoContent.title) {
+    errors.push("title");
+  }
 
-    if (!videoContent.originTitle) {
-        errors.push('originTitle');
-    }
+  if (!videoContent.originTitle) {
+    errors.push("originTitle");
+  }
 
-    if (!videoContent.typeVideoContent) {
-        errors.push('typeVideoContent');
-    } else if (!['movies', 'cartoons', 'serials', 'cartoon-series'].includes(videoContent.typeVideoContent)) {
-        errors.push('typeVideoContent (must be movies', 'cartoons', 'serials', 'cartoon-series)');
-    }
+  if (!videoContent.typeVideoContent) {
+    errors.push("typeVideoContent");
+  } else if (
+    !["movies", "cartoons", "serials", "cartoon-series"].includes(
+      videoContent.typeVideoContent
+    )
+  ) {
+    errors.push(
+      "typeVideoContent (must be movies",
+      "cartoons",
+      "serials",
+      "cartoon-series)"
+    );
+  }
 
+  if (!videoContent.IMDb) {
+    errors.push("IMDb");
+  }
 
-    if (!videoContent.IMDb) {
-        errors.push('IMDb');
-    }
+  if (!videoContent.description) {
+    errors.push("description");
+  }
 
-    if (!videoContent.description) {
-        errors.push('description');
-    }
+  if (!videoContent.releaseDate) {
+    errors.push("releaseDate videoContent");
+  }
 
-    if (!videoContent.releaseDate) {
-        errors.push('releaseDate videoContent');
-    }
+  if (!videoContent.duration) {
+    errors.push("duration");
+  }
 
-    if (!videoContent.duration) {
-        errors.push('duration');
-    }
+  if (!videoContent.previewURL) {
+    errors.push("preview videoContent");
+  }
 
-    if (!videoContent.previewURL) {
-        errors.push('preview videoContent');
-    }
+  if (!videoContent.originCountries) {
+    errors.push("originCountries");
+  } else if (!Array.isArray(videoContent.originCountries)) {
+    errors.push("originCountries");
+  } else if (videoContent.originCountries.length == 0) {
+    errors.push("originCountries (must be 1 country)");
+  }
 
-    if (!videoContent.originCountries) {
-        errors.push('originCountries');
-    } else if (!Array.isArray(videoContent.originCountries)) {
-        errors.push('originCountries');
-    } else if (videoContent.originCountries.length == 0) {
-        errors.push('originCountries (must be 1 country)');
-    }
+  if (!videoContent.genres) {
+    errors.push("genres");
+  } else if (!Array.isArray(videoContent.genres)) {
+    errors.push("genres");
+  } else if (videoContent.genres.length == 0) {
+    errors.push("genres (must be 1 genre)");
+  }
 
-    if (!videoContent.genres) {
-        errors.push('genres');
-    } else if (!Array.isArray(videoContent.genres)) {
-        errors.push('genres');
-    } else if (videoContent.genres.length == 0) {
-        errors.push('genres (must be 1 genre)');
-    }
+  if (!videoContent.actors) {
+    errors.push("actors");
+  } else if (!Array.isArray(videoContent.actors)) {
+    errors.push("actors");
+  } else if (videoContent.actors.length == 0) {
+    errors.push("actors (must be 1 actor)");
+  }
 
-    if (!videoContent.actors) {
-        errors.push('actors');
-    } else if (!Array.isArray(videoContent.actors)) {
-        errors.push('actors');
-    } else if (videoContent.actors.length == 0) {
-        errors.push('actors (must be 1 actor)');
-    }
+  if (!videoContent.directors) {
+    errors.push("directors");
+  } else if (!Array.isArray(videoContent.directors)) {
+    errors.push("directors");
+  } else if (videoContent.directors.length == 0) {
+    errors.push("directors (must be 1 director)");
+  }
 
-    if (!videoContent.directors) {
-        errors.push('directors');
-    } else if (!Array.isArray(videoContent.directors)) {
-        errors.push('directors');
-    } else if (videoContent.directors.length == 0) {
-        errors.push('directors (must be 1 director)');
-    }
+  if (!videoContent.lists) {
+    errors.push("lists");
+  } else if (!Array.isArray(videoContent.lists)) {
+    errors.push("lists");
+  }
 
-    if (!videoContent.lists) {
-        errors.push('lists');
-    } else if (!Array.isArray(videoContent.lists)) {
-        errors.push('lists');
-    }
+  if (videoContent.soundTracks && !Array.isArray(videoContent.soundTracks)) {
+    errors.push("soundTracks");
+  }
 
-    if (videoContent.soundTracks && !Array.isArray(videoContent.soundTracks)) {
-        errors.push('soundTracks');
-    }
+  if (videoContent.seasons && !Array.isArray(videoContent.seasons)) {
+    errors.push("seasons");
+  }
 
-    if (videoContent.seasons && !Array.isArray(videoContent.seasons)) {
-        errors.push('seasons');
-    }
-
-    emitErrors(errors);
-}
+  emitErrors(errors);
+};
 
 const validateActorDirector = (person: Actor | Director) => {
-    const errors: string[] = [];
+  const errors: string[] = [];
 
-    if (!person.firstName) {
-        errors.push('firstName');
-    }
+  if (!person.firstName) {
+    errors.push("firstName");
+  }
 
-    if (!person.lastName) {
-        errors.push('lastName');
-    }
-    if (!person.originalFullName) {
-        errors.push('originalFullName');
-    }
-    if (!person.photoURL) {
-        errors.push('photoURL');
-    }
+  if (!person.lastName) {
+    errors.push("lastName");
+  }
+  if (!person.originalFullName) {
+    errors.push("originalFullName");
+  }
+  if (!person.photoURL) {
+    errors.push("photoURL");
+  }
 
-    if (!person.sex) {
-        errors.push('sex');
-    }
+  if (!person.sex) {
+    errors.push("sex");
+  }
 
-    if (!person.placeBirth) {
-        errors.push('placeBirth');
-    }
+  if (!person.placeBirth) {
+    errors.push("placeBirth");
+  }
 
-    emitErrors(errors);
-}
+  emitErrors(errors);
+};
 
 const validateRoom = (room: Room) => {
-    const errors: string[] = [];
+  const errors: string[] = [];
 
-    if (!room.ownerId) {
-        errors.push('ownerId');
-    }
+  if (!room.ownerId) {
+    errors.push("ownerId");
+  }
 
-    if (!room.videoContentId) {
-        errors.push('videoContentId');
-    }
+  if (!room.videoContentId) {
+    errors.push("videoContentId");
+  }
 
-    if (!room.title) {
-        errors.push('title');
-    }
+  if (!room.title) {
+    errors.push("title");
+  }
 
-    if (!room.description) {
-        errors.push('description');
-    }
+  if (!room.description) {
+    errors.push("description");
+  }
 
-    emitErrors(errors);
-}
+  emitErrors(errors);
+};
 
 const validateMessage = (message: Message) => {
-    const errors: string[] = [];
+  const errors: string[] = [];
 
-    if (!message.roomId) {
-        errors.push('roomId');
-    }
+  if (!message.roomId) {
+    errors.push("roomId");
+  }
 
-    if (!message.userId) {
-        errors.push('userId');
-    }
+  if (!message.userId) {
+    errors.push("userId");
+  }
 
-    if (!message.message) {
-        errors.push('message');
-    }
+  if (!message.message) {
+    errors.push("message");
+  }
 
-    emitErrors(errors);
-}
+  emitErrors(errors);
+};
+
+const validateTypeContent = (typeContent: TypeContent) => {
+  const errors: string[] = [];
+
+  if (!typeContent.name) {
+    errors.push("name");
+  }
+
+  if (!typeContent.path) {
+    errors.push("path");
+  }
+
+  emitErrors(errors);
+};
 
 export {
-    validateVideoContent,
-    validateActorDirector,
-    validateRoom,
-    validateMessage,
+  validateVideoContent,
+  validateActorDirector,
+  validateRoom,
+  validateMessage,
+  validateTypeContent,
 };
