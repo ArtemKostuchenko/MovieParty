098a04ed08bf4085eef6cc7bce27f1cdb5587f45
KostArt <ipz204_kav@student.ztu.edu.ua>
2024-05-19 16:27:15 +0300

    Created usePart hook for parts

---
+++ frontend/src/hooks/usePart.jsx
@@ -0,0 +1,44 @@
+import {
+  useAddPartMutation,
+  useUpdatePartMutation,
+  useRemovePartMutation,
+} from "../features/services/parts/partsService";
+
+const usePart = () => {
+  const [addMutation, { isLoading: isLoadingAdd, isSuccess: isSuccessAdd }] =
+    useAddPartMutation();
+
+  const [
+    updateMutation,
+    { isLoading: isLoadingUpdate, isSuccess: isSuccessUpdate },
+  ] = useUpdatePartMutation();
+
+  const [
+    removeMutation,
+    { isLoading: isLoadingRemove, isSuccess: isSuccessRemove },
+  ] = useRemovePartMutation();
+
+  const addPart = async (genre) => {
+    return await addMutation(genre).unwrap();
+  };
+
+  const updatePart = async (genre) => {
+    return await updateMutation(genre).unwrap();
+  };
+
+  const removePart = async (id) => {
+    return await removeMutation(id).unwrap();
+  };
+
+  return {
+    addPart,
+    isLoadingAdd,
+    isSuccessAdd,
+    updatePart,
+    isLoadingUpdate,
+    isSuccessUpdate,
+    removePart,
+    isLoadingRemove,
+    isSuccessRemove,
+  };
+};
