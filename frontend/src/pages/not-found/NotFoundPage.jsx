5a40a150a665a81cf25830167f3271a252f25c74
KostArt <ipz204_kav@student.ztu.edu.ua>
2024-06-11 22:18:16 +0300

    Update NotFoundPage when error status is 500

--- frontend/src/pages/not-found/NotFoundPage.jsx
+++ frontend/src/pages/not-found/NotFoundPage.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 import "./style.page.scss";
 import { NotFound } from "../../components";
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
+  const errors = useRouteError();
+
+  const { status } = errors;
+
+  if (status === 404) {
+    return (
+      <div className="not-found-page">
+        <NotFound
+          title="404 Сторінку не знайдено"
+          description="Такої сторінки не існує чи вона є не доступною"
+          splitter={false}
+        />
+        <button className="button additional" onClick={() => navigate("/")}>
+          На головну
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="not-found-page">
       <NotFound
-        title="404 Сторінку не знайдено"
-        description="Такої сторінки не існує чи вона є не доступною"
+        title="500 Внутрішня помилка сервера"
+        description="Щось пішло не так, спробуйте будь-ласка пізніше"
         splitter={false}
       />
       <button className="button additional" onClick={() => navigate("/")}>
