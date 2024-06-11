9b661f5bc55594fe08aa99d8f0f899d7c68158a7
KostArt <ipz204_kav@student.ztu.edu.ua>
2024-06-11 22:07:32 +0300

    Fixed Admin route, after log in

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import useUser from "../hooks/useUser";
 import { Navigate } from "react-router-dom";
+import Loader from "./Loader/Loader";
 
 const AdminRoute = ({ children }) => {
   const { user } = useUser();
+
+  if (!user) {
+    return <Loader fixed />;
+  }
 
   if (!user.isAdmin) {
     return <Navigate to="/" replace state={{ from: location }} />;
--- frontend/src/components/SubscriptionRoute.jsx
+++ frontend/src/components/SubscriptionRoute.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import useSubscription from "../hooks/useSubscription";
-import Loader from "./Loader/Loader";
 
 const SubscriptionRoute = ({ children }) => {
   const location = useLocation();
