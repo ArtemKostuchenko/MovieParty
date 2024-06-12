1b80592b946e958129b3a1c84dc5b828d6e03f86
KostArt <ipz204_kav@student.ztu.edu.ua>
2024-06-12 16:25:32 +0300

    Remove log in email.ts

--- backend/src/utils/email/email.ts
+++ backend/src/utils/email/email.ts
@@ -4,51 +4,55 @@ import path from "path";
 import fs from "fs";
 
 interface EmailOptions {
-    from: string | undefined;
-    to: string;
-    subject: string;
-    html: string;
+  from: string | undefined;
+  to: string;
+  subject: string;
+  html: string;
 }
 
 const sendMail = async (transporter: Transporter, options: EmailOptions) => {
-    try {
-        await transporter.sendMail(options);
-        console.log('Mail is sent!');
-    } catch (err) {
-        console.log(err);
-        throw err;
-    }
-}
+  try {
+    await transporter.sendMail(options);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
 
-const sendEmail = async (email: string, subject: string, payload: any, template: string) => {
-    try {
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            host: process.env.EMAIL_HOST as string,
-            port: 587,
-            secure: false,
-            auth: {
-                user: process.env.EMAIL_USER as string,
-                pass: process.env.EMAIL_PASS as string,
-            },
-        });
+const sendEmail = async (
+  email: string,
+  subject: string,
+  payload: any,
+  template: string
+) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: process.env.EMAIL_HOST as string,
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER as string,
+        pass: process.env.EMAIL_PASS as string,
+      },
+    });
 
-        const source = fs.readFileSync(path.join(__dirname, template), "utf8");
-        const compiledTemplate = handlebars.compile(source);
-        const htmlContent = compiledTemplate(payload);
+    const source = fs.readFileSync(path.join(__dirname, template), "utf8");
+    const compiledTemplate = handlebars.compile(source);
+    const htmlContent = compiledTemplate(payload);
 
-        const options: EmailOptions = {
-            from: process.env.EMAIL_USER,
-            to: email,
-            subject: subject,
-            html: htmlContent,
-        };
+    const options: EmailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: subject,
+      html: htmlContent,
+    };
 
-        await sendMail(transporter, options);
-        return 'Email sent successfully!';
-    } catch (error) {
-        throw error;
-    }
+    await sendMail(transporter, options);
+    return "Email sent successfully!";
+  } catch (error) {
+    throw error;
+  }
 };
 
 export { sendEmail };
